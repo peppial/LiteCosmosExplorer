f41cee5c3046ecdc33654120717affaa0640427e
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2023-12-29 22:53:13 +0200

    Added DI

--- CosmosExplorer.Domain/CosmosDBDocumentService.cs
+++ CosmosExplorer.Domain/CosmosDBDocumentService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
-using CosmosExplorer.Core;
+﻿using CosmosExplorer.Core;
 using CosmosExplorer.Core.Command;
 using CosmosExplorer.Core.Query;
 using CosmosExplorer.Core.Connection;
