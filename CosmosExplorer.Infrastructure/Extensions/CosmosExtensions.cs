28773533e8bef31578d8f8c1f780f0b091ba1dd9
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2024-06-24 17:41:35 +0300

    Add extension and fix ReplaceItem

--- CosmosExplorer.Infrastructure/Extensions/CosmosExtensions.cs
+++ CosmosExplorer.Infrastructure/Extensions/CosmosExtensions.cs
@@ -17,4 +17,26 @@ public class CosmosExtensions
 
         return builder.Build();
     }
+    
+    public static PartitionKey GetPartitionKeyNullable(Partition partition)
+    {
+        PartitionKeyBuilder builder = new();
+
+        if (partition.PartitionName1 is not null)
+        {
+            builder.Add(partition.PartitionName1);
+        }
+
+        if (partition.PartitionName2 is not null)
+        {
+            builder.Add(partition.PartitionName2);
+        }
+
+        if (partition.PartitionName3 is not null)
+        {
+            builder.Add(partition.PartitionName3);
+        }
+
+        return builder.Build();
+    }
 }
