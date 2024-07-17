a1284bd57c58eaba60f2f807bfc2ca72796b1abf
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2024-07-17 15:13:39 +0300

    Update BuildMacOSX64.ps1

--- scripts/BuildMacOSX64.ps1
+++ scripts/BuildMacOSX64.ps1
@@ -1,12 +1,12 @@
 [string] $Product = "LiteCosmosExplorer"
 [string] $Target = "MacOSX64"
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true" -ProjectPath "CosmosExplorer.Avalonia\CosmosExplorer.Avalonia.csproj"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -BuildArgs "-t:BundleApp -p:RuntimeIdentifier=osx-x64 -p:IncludeAllContentForSelfExtract=true" -ProjectPath "CosmosExplorer.Avalonia\CosmosExplorer.Avalonia.csproj"
 
 # Remove everything except the app bundle
 Get-Childitem "$PSScriptRoot\..\bld\$Product.$Target\" -Exclude "LiteCosmosExplorer.app" | Remove-Item -Recurse
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -Clean $False -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -Clean $False -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-x64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true"
 
 # Remove unbundled 
 Remove-Item "$PSScriptRoot\..\bld\$Product.$Target\LiteCosmosExplorer"
