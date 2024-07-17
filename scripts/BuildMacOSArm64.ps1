fbbc0d5d4dc2e46fd0d67674717f2a3be8e313a5
Penka Alexandrova <penka.alexandrova@scaleforce.io>
2024-07-17 15:08:50 +0300

    Update BuildMacOSArm64.ps1

--- scripts/BuildMacOSArm64.ps1
+++ scripts/BuildMacOSArm64.ps1
@@ -6,7 +6,7 @@
 # Remove everything except the app bundle
 Get-Childitem "$PSScriptRoot\..\bld\$Product.$Target\" -Exclude "LiteCosmosExplorer.app" | Remove-Item -Recurse
 
-& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -Clean $False -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-arm64 -p:PublishSingleFile=true -p:PublishTrimmed=true -p:IncludeAllContentForSelfExtract=true -p:EnableCompressionInSingleFile=true"
+& "$PSScriptRoot\Build.ps1" -Product $Product -Target $Target -Clean $False -BuildArgs "-t:Publish -p:RuntimeIdentifier=osx-arm64 -p:PublishSingleFile=true -p:IncludeAllContentForSelfExtract=true"
 
 # Remove unbundled 
 Remove-Item "$PSScriptRoot\..\bld\$Product.$Target\LiteCosmosExplorer"
